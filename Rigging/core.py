f0abf54c9a3831e00200b321bd96974bb071c8f3
Pritish <pritish.dogra@gmail.com>
2015-07-29 19:22:46 +1200

    -Successfully implemented Red9 into subclasses. Now works even if you open and close maya files.

--- Rigging/core.py
+++ Rigging/core.py
@@ -21,8 +21,10 @@ def _fullSide_(side):
                 }
     return sideDict[side]
 
+
 class PKD_MetaClass(Red9_Meta.MetaClass):
     pass
+
 
 class PKD_Meta(Red9_Meta.MetaRig):
     def __init__(self, *args, **kws):
@@ -35,9 +37,8 @@ class PKD_Meta(Red9_Meta.MetaRig):
         self.addAttr('mirrorSide', enumName='Centre:Left:Right:Unique',
                      attrType='enum', hidden=True)  # ensure these are added by default
 
-    def etParent(self,targetSystem):
+    def etParent(self, targetSystem):
         self.pynode.setParent(targetSystem.mNode)
-
 
 
 def _add_meta_data_(node, type):
@@ -57,10 +58,11 @@ class SubSystem(Red9_Meta.MetaRigSubSystem):
             self.lockState = False
             self.lockState = False
         else:
-            super(SubSystem, self).__init__( *args, **kws)
+            super(SubSystem, self).__init__(*args, **kws)
 
-    def setParent(self,targetSystem):
+    def setParent(self, targetSystem):
         self.pynode.setParent(targetSystem.mNode)
+
 
 class Ctrl(PKD_Meta):
     """This is a base control System"""
@@ -70,30 +72,36 @@ class Ctrl(PKD_Meta):
             full_name = utils.nameMe(kws["side"], kws["part"], "Ctrl")
             super(Ctrl, self).__init__(name=full_name, nodeType="transform")
             self.part = kws["part"]
-            self.side = kws["side"]
-            self.ctrl = self
-            self.prnt = None
-            self.xtra = None
-            self.constrainNode = None
-            self.hasConstrainNode = False
-            self.ctrlShape = "Ball"
-            self.parentMasterSN = None
-            self.parentMasterPH = None
-            self.hasParentMaster = False
             # Set Meta Attr
             self.rigType = "Ctrl"
-            self.mirrorSide = _fullSide_(self.side)
+            self.mirrorSide = _fullSide_(kws["side"])
             self.mSystemRoot = False
         else:
-            super(Ctrl, self).__init__( *args, **kws)
+            super(Ctrl, self).__init__(*args, **kws)
 
+        self.ctrl = self
+        self._prnt_ = None
+        # Internal Var
+        self._xtra_ = None
+        self._side_ = None
+        self._gimbal_ = None
+        self._parentMasterPH_ = None
+        self._parentMasterSN_ = None
+        self.hasConstrainNode = False
+        self.ctrlShape = "Ball"
+        self.hasParentMaster = False
+        # self._internal_var_ = {"Prnt":self._prnt_,
+        #                        "Xtra":self._xtra_,
+        #                        "Gimbal":self._gimbal_,
+        #
+        #
+        #                        }
 
     def __bindData__(self):
+        """Set up the part attributes"""
         super(Ctrl, self).__bindData__()
-        self.addAttr("part","")
-        self.attrSetLocked("part",True)
-
-
+        self.addAttr("part", "")
+        self.attrSetLocked("part", True)
 
     def create_ctrl(self):
         # Create the xtra grp
@@ -135,35 +143,104 @@ class Ctrl(PKD_Meta):
         self.pynode.setParent(self.parentMasterSN.mNode)
         self.parentMasterSN.pynode.setParent(self.parentMasterPH.mNode)
         self.parentMasterPH.pynode.setParent(self.xtra.mNode)
-        # Set the state of the parent master
-        self.hasParentMaster = True
+        # Add support node
+        self.addSupportNode(self.parentMasterSN, "ParentMasterSN")
+        self.addSupportNode(self.parentMasterPH, "ParentMasterPH")
 
     def add_constrain_node(self):
-        self.constrainNode = PKD_Meta(name=utils.nameMe(self.side, self.part, "Constrain"), nodeType="transform")
-        self.constrainNode.rigType = "constrain"
-        self.constrainNode.pynode.setParent(self.mNode)
-
+        self.gimbal = PKD_Meta(name=utils.nameMe(self.side, self.part, "Gimbal"), nodeType="transform")
+        self.gimbal.rigType = "constrain"
+        self.gimbal.pynode.setParent(self.mNode)
         self.hasConstrainNode = True
+        self.addSupportNode(self.gimbal, "Gimbal")
 
-    def setParent(self,targetSystem):
-
+    def setParent(self, targetSystem):
         # print isinstance (targetSystem, Red9_Meta.MetaClass)
         self.prnt.pynode.setParent(targetSystem.mNode)
 
+    def _get_initialise_internal_(self, internalVariableName):
+        # Check that there is connection
+        if self.__dict__[internalVariableName] is None:
+            # Look for the connection
+            nodeAttrName = "SUP_%s" % internalVariableName.replace("_", "").capitalize()
+            if self.hasAttr(nodeAttrName):
+                # Initialise the parent class
+                self.__dict__[internalVariableName] = eval("self.%s" % nodeAttrName)
+        return self.__dict__[internalVariableName]
+
+    def _set_initialise_internal_(self, internalVariableName, data):
+        try:
+            assert isinstance(data, Red9_Meta.MetaClass)
+            self.__dict__[internalVariableName] = data
+        except:
+            raise Exception("Input must be MetaClass")
+
+    @property
+    def side(self):
+        return self.pynode.mirrorSide.get(asString=True)[0]
+
+    @property
+    def prnt(self):
+        return self._get_initialise_internal_("_prnt_")
+
+    @prnt.setter
+    def prnt(self, data):
+        self._set_initialise_internal_("_prnt_", data)
+
+    @property
+    def xtra(self):
+        return self._get_initialise_internal_("_xtra_")
+
+    @xtra.setter
+    def xtra(self, data):
+        self._set_initialise_internal_("_xtra_", data)
+
+    @property
+    def gimbal(self):
+        return self._get_initialise_internal_("_gimbal_")
+
+    @gimbal.setter
+    def gimbal(self, data):
+        data = self._set_initialise_internal_("_gimbal_", data)
+        if data is not None:
+            self.hasConstrainNode = True
+        return data
+
+    @property
+    def parentMasterPH(self):
+        data = self._get_initialise_internal_("_parentMasterPH_")
+        if data is not None:
+            self.hasParentMaster = True
+        return data
+
+    @parentMasterPH.setter
+    def parentMasterPH(self, data):
+        self._set_initialise_internal_("_parentMasterPH_", data)
+
+    @property
+    def parentMasterSN(self):
+        return self._get_initialise_internal_("_parentMasterSN_")
+
+    @parentMasterSN.setter
+    def parentMasterSN(self, data):
+        self._set_initialise_internal_("_parentMasterSN_", data)
 
 
 class Anno_Loc(Red9_Meta.MetaClass):
     """This is a a annoated locator"""
     pass
 
+
 class MyCameraMeta(Red9_Meta.MetaClass):
-    '''
+    """
     Example showing that metaData isn't limited to 'network' nodes,
     by using the 'nodeType' arg in the class __init__ you can modify
     the general behaviour such that meta creates any type of Maya node.
-    '''
-    def __init__(self,*args,**kws):
-        super(MyCameraMeta, self).__init__(nodeType='camera',*args,**kws)
+    """
+
+    def __init__(self, *args, **kws):
+        super(MyCameraMeta, self).__init__(nodeType='camera', *args, **kws)
+
 
 Red9_Meta.registerMClassInheritanceMapping()
 Red9_Meta.registerMClassNodeMapping(nodeTypes='transform')
@@ -172,32 +249,39 @@ Red9_Meta.registerMClassNodeMapping(nodeTypes='camera')
 if __name__ == '__main__':
     pm.newFile(f=1)
     # cam = MyCameraMeta()
-    subSystem = SubSystem(side = "U", part = "Core")
+    subSystem = SubSystem(side="U", part="Core")
 
-    mRig = Red9_Meta.MetaRig(name='CharacterRig',nodeType="transform")
+    mRig = Red9_Meta.MetaRig(name='CharacterRig', nodeType="transform")
     mRig.connectChild(subSystem, 'Arm')
     subSystem.setParent(mRig)
 
-
-    fkSystem = SubSystem(side = "U", part = "FK")
+    fkSystem = SubSystem(side="U", part="FK")
 
     fkSystem.setParent(subSystem)
     subSystem.connectChild(fkSystem, 'FK_System')
 
-
-    myCtrl = Ctrl(side = "U", part = "FK0")
+    myCtrl = Ctrl(side="U", part="FK0")
     myCtrl.create_ctrl()
-    myCtrl.add_parent_master()
     myCtrl.add_constrain_node()
+    myCtrl.add_parent_master()
     myCtrl.setParent(fkSystem)
 
-    myCtrl1 = Ctrl(side ="U", part = "FK1")
+    myCtrl1 = Ctrl(side="U", part="FK1")
     myCtrl1.create_ctrl()
 
     myCtrl1.setParent(fkSystem)
 
     fkCtrls = [myCtrl.mNode, myCtrl1.mNode]
-    fkSystem.connectChildren(fkCtrls,"Ctrl")
-    subSystem.connectChildren(fkCtrls,"FK")
+    fkSystem.connectChildren(fkCtrls, "Ctrl")
+    subSystem.connectChildren(fkCtrls, "FK")
 
 
+    # Need to run this in case of opening and closing file
+
+    # from PKD_Tools.Red9 import Red9_Meta
+    # reload(Red9_Meta)
+    #
+    # from PKD_Tools.Rigging import core
+    # reload(core)
+    #
+    # k = Red9_Meta.MetaClass("CharacterRig")
--- Rigging/utils.py
+++ Rigging/utils.py
@@ -10,24 +10,26 @@ reload(libFile)
 def nameMe(partSfx, partName, endSuffix):
     """Set the name convention of all nodes"""
     if partSfx and partName and endSuffix:
-        return "%s_%s_%s"%(partSfx,partName,endSuffix)
+        return "%s_%s_%s" % (partSfx, partName, endSuffix)
 
-ctrlJsonFile = libFile.join(libFile.current_working_directory(),"Rigging/Data/Ctrl.json")
+
+ctrlJsonFile = libFile.join(libFile.current_working_directory(), "Rigging/Data/Ctrl.json")
+
 
 def export_ctrl_shapes():
     """Export curve data from the existing curve to a json file"""
-    curvesData= {}
+    curvesData = {}
     for top in pm.ls(assemblies=True, ud=True):
-        if top.getShape().type()=="nurbsCurve":
-            detailedInfo = {}
-            detailedInfo["cvs"] = [list(cv) for cv in top.getCVs()]
-            detailedInfo["form"] = top.f.get()
-            detailedInfo["degree"] = top.degree()
-            detailedInfo["knots"] = top.numKnots()
+        if top.getShape().type() == "nurbsCurve":
+            detailedInfo = {"cvs": [list(cv) for cv in top.getCVs()],
+                            "form": top.f.get(),
+                            "degree": top.degree(),
+                            "knots": top.numKnots()}
             curvesData[top.name()] = detailedInfo
 
-    libFile.write_json(ctrlJsonFile,curvesData)
-    pyLog.info("Curve information written to: %s"%ctrlJsonFile)
+    libFile.write_json(ctrlJsonFile, curvesData)
+    pyLog.info("Curve information written to: %s" % ctrlJsonFile)
+
 
 def build_ctrl_shape(type=""):
     """Create a curve from the json"""
@@ -35,19 +37,18 @@ def build_ctrl_shape(type=""):
 
     if curvesData.has_key(type):
         detailedInfo = curvesData[type]
-        return pm.curve(name = type,
-                 point=detailedInfo["cvs"],
-                 per=detailedInfo["form"],
-                 k=range(detailedInfo["knots"]),
-                 degree=detailedInfo["degree"])
+        return pm.curve(name=type,
+                        point=detailedInfo["cvs"],
+                        per=detailedInfo["form"],
+                        k=range(detailedInfo["knots"]),
+                        degree=detailedInfo["degree"])
 
     else:
-        pyLog.warning("%s not found in exported curve information file"%type)
+        pyLog.warning("%s not found in exported curve information file" % type)
+
 
 def build_all_ctrls_shapes():
     """Build all curves from the json file"""
     curvesData = libFile.load_json(ctrlJsonFile)
     for crv in curvesData.keys():
         build_ctrl_shape(crv)
-
-
