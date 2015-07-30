c4158ae9c6e3df4e2549b00093b6f58ff070dd35
pritishd <pritish.dogra@gmail.com>
2015-07-30 23:36:30 +1200

    - Further development of meta unit test

--- UnitTests/metaNetworkUnitTest.py
+++ UnitTests/metaNetworkUnitTest.py
@@ -5,8 +5,16 @@ from PKD_Tools import libUnitTests
 from PKD_Tools.Red9 import Red9_Meta
 from PKD_Tools.Rigging import core
 
-for module in [libUnitTests, Red9_Meta, core]:
-    reload(module)
+# for module in [libUnitTests, Red9_Meta, core]:
+#     reload(module)
+
+
+class UnitTestCase(libUnitTests.UnitTestCase):
+    """Base Class For All Unit Test."""
+
+    def test_meta_inheritance(self, targetObject, node):
+        """Test Inheritence of meta classes"""
+        self.assertTrue(issubclass(targetObject, Red9_Meta.MetaClass))
 
 
 class Droid(object):
@@ -36,6 +44,7 @@ class Droid(object):
         subSystem.connectChild(fkSystem, 'FK_System')
 
         self.myCtrl = core.Ctrl(side="U", part="FK0")
+        self.myCtrl.create_ctrl()
         self.myCtrl.setParent(fkSystem)
 
     def reinitialise_meta_network(self):
@@ -61,11 +70,25 @@ class BatchTest(libUnitTests.BatchTest):
         super(BatchTest, self).__init__()
         self.droid = Droid()
 
-    def test_meta_open_close(self):
-        self.droid.create_simple_ctrl_meta_network()
 
+    def addTest(self, testName, **kwargs):
+        # Generalised function to add a test to a suite
+        self.suite.addTest(UnitTestCase(testName, **kwargs))
+
+
+    def test_meta_create(self):
+        self.droid.create_simple_ctrl_meta_network()
+        # Setup a batch test suite
+        self.suite = libUnitTests.unittest.TestSuite()
+        self.addTest("test_meta_inheritance", targetObject=self.droid.mRig, item="Master Rig Group")
+        self.addTest("test_meta_inheritance", targetObject=self.droid.myCtrl, item="Control Group")
+        self.run_test("Testing meta creation")
+    # print "Generalised Tests"
+# unit = BatchTest()
+# unit.test_meta_create()
 
 if __name__ == '__main__':
+    print "######################"
     print "Generalised Tests"
     unit = BatchTest()
-    unit.batch_test()
+    unit.test_meta_create()
